ecfeafc54fc54d72db695ae2d5c6e264903793ee
Sergey Cherkasov <iamcherkasov.job@gmail.com>
2025-10-08 23:16:22 +0700

    GH-159: migrate to YAML OpenAPI spec from Annotations (Environment, Info, software-api) (#159)

--- master/src/main/java/com/nucleonforge/axile/master/api/InfoApi.java
+++ master/src/main/java/com/nucleonforge/axile/master/api/InfoApi.java
@@ -2,14 +2,6 @@ package com.nucleonforge.axile.master.api;
 
 import java.util.Objects;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.links.Link;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nucleonforge.axile.common.api.info.ServiceInfo;
 import com.nucleonforge.axile.common.domain.http.NoHttpPayload;
-import com.nucleonforge.axile.master.api.error.SimpleApiError;
 import com.nucleonforge.axile.master.api.response.info.InfoResponse;
 import com.nucleonforge.axile.master.model.instance.InstanceId;
 import com.nucleonforge.axile.master.service.convert.Converter;
@@ -28,9 +19,6 @@ import com.nucleonforge.axile.master.service.transport.InfoEndpointProber;
  *
  * @author Sergey Cherkasov
  */
-@Tag(
-        name = "General instance information API",
-        description = "The info endpoint provides general information about the particular Spring Boot instance")
 @RestController
 @RequestMapping(path = ApiPaths.InfoApi.MAIN)
 public class InfoApi {
@@ -43,37 +31,6 @@ public class InfoApi {
         this.converter = converter;
     }
 
-    @Operation(
-            summary = "Returns general information about the instance.",
-            responses = {
-                @ApiResponse(
-                        description = "OK",
-                        responseCode = "200",
-                        links = {
-                            @Link(
-                                    name = "Actuator/Info",
-                                    description = "https://docs.spring.io/spring-boot/api/rest/actuator/info.html")
-                        },
-                        content =
-                                @Content(
-                                        mediaType = "application/json",
-                                        schema = @Schema(implementation = InfoResponse.class))),
-                @ApiResponse(
-                        description = "Bad Request",
-                        responseCode = "400",
-                        content =
-                                @Content(
-                                        mediaType = "application/json",
-                                        schema = @Schema(implementation = SimpleApiError.class))),
-                @ApiResponse(
-                        description = "Internal Server Error",
-                        responseCode = "500",
-                        content =
-                                @Content(
-                                        mediaType = "application/json",
-                                        schema = @Schema(implementation = SimpleApiError.class)))
-            })
-    @Parameter(name = "instanceId", description = "Application Instance ID", required = true)
     @GetMapping(path = ApiPaths.InfoApi.INSTANCE_ID)
     public InfoResponse getInfoResponse(@PathVariable("instanceId") String instanceId) {
         ServiceInfo serviceInfo = infoEndpointProber.invoke(InstanceId.of(instanceId), NoHttpPayload.INSTANCE);
