2a2642633665c5f86a1aff5d35c36562200ced86
Mikhail Polivakha <mikhailpolivakha@email.com>
2025-11-14 22:56:50 +0300

    GH-314: Polishing build file in common utils

--- common/utils/build.gradle.kts
+++ common/utils/build.gradle.kts
@@ -1,19 +1,3 @@
 plugins {
-    id("java")
-}
-
-group = "com.nucleonforge.axile"
-version = "1.0.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    id("common")
 }
