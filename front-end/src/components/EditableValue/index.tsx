b5c905cdac906b8b0d295ee7331227377270b39e
Ashot <ashotsargsyan527@gmail.com>
2026-02-05 17:35:44 +0400

    GH-666 Created a tooltip that explains the cron expression

--- front-end/src/components/EditableValue/index.tsx
+++ front-end/src/components/EditableValue/index.tsx
@@ -20,6 +20,7 @@ import { CheckOutlined, CloseOutlined, EditOutlined } from "@ant-design/icons";
 import { Button, Input } from "antd";
 import { useState } from "react";
 
+import { CronExpressionOptionalTooltip } from "./CronExpressionOptionalTooltip";
 import styles from "./styles.module.css";
 
 interface IProps {
@@ -43,6 +44,12 @@ interface IProps {
      * @param value the new value after change.
      */
     onNewValue: (value: string) => void;
+
+    /**
+     * Function to generate a tooltip for the value.
+     * Receives the current value and returns a string to display.
+     */
+    tooltipFormatter?: (value: string) => string;
 }
 
 export const EditableValue = ({
@@ -50,6 +57,7 @@ export const EditableValue = ({
     onNewValue,
     className = styles.DefaultPropertyValueWrapper,
     editClassName = styles.DefaultEditPropertyWrapper,
+    tooltipFormatter,
 }: IProps) => {
     // TODO:
     //  We need to improve this component:
@@ -61,7 +69,10 @@ export const EditableValue = ({
     if (!editingValue) {
         return (
             <div className={className}>
-                {actualValue}
+                <CronExpressionOptionalTooltip value={actualValue} tooltipFormatter={tooltipFormatter}>
+                    {actualValue}
+                </CronExpressionOptionalTooltip>
+
                 <Button
                     icon={<EditOutlined />}
                     type="primary"
@@ -74,11 +85,13 @@ export const EditableValue = ({
 
     return (
         <div className={editClassName}>
-            <Input
-                value={actualValue}
-                onChange={(e) => setActualValue(e.target.value)}
-                className={styles.EditPropertyField}
-            />
+            <CronExpressionOptionalTooltip value={actualValue} tooltipFormatter={tooltipFormatter}>
+                <Input
+                    value={actualValue}
+                    onChange={(e) => setActualValue(e.target.value)}
+                    className={styles.EditPropertyField}
+                />
+            </CronExpressionOptionalTooltip>
 
             <Button
                 icon={<CloseOutlined />}
